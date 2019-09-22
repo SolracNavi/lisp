364d13099f0703b44f1eef3ff93087a862a80cd4
carl <el.sariri@gmail.com>
2019-09-22 15:12:58 -0300

    Eliminando archivos

--- Juegos/option-9-master/sparks.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :option-9)
-
-#+option-9-debug (declaim (optimize (safety 3) (space 0) (speed 0) (debug 3)))
-
-(defmethod sparks ((ent entity))
-  (+ (initial-sparks ent) (random (additional-sparks ent))))
-
